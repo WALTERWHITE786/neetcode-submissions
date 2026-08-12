67756e9e000d9e53b09a4bae481a72b4c06cd8b0
WALTERWHITE786 <wasimsharafath@gmail.com>
2026-08-12 21:28:46 +0530

    Bulk sync: 4 submissions

---
+++ Data Structures & Algorithms/two-integer-sum/submission-2.py
@@ -0,0 +1,11 @@
+class Solution:
+    def twoSum(self,nums:list[int],target:int):
+        d={}
+        for i,x in enumerate(nums):
+            if target-x in d:
+                return [d[target-x],i]
+            d[x]=i
+
+
+
+
